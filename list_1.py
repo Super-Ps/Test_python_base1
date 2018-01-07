338c19f86aaefef78afacfb967ffd600d921af77
jonnypeng <3433201@qq.com>
2018-01-07 17:23:12 +0800

    list 提交

---
+++ list_1.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# __author__ = "Jonny"
+# Email: jonnysps@yeah.net
+# Date: 2018/1/7
+
+
+
+list = [1,2,1,2,1,2,1,2]
+
+for x in list:
+
+    if x ==1:
+        list.remove(x)
+print(list)
+
+
+
+list = [1,2,1,2,1,1,1]
+
+for x in list:
+
+    if x ==1:
+        list.remove(x)
+print(list)
